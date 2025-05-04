57acc7471392a8473c66c0bdfa71774ae490d7d5
LukasGP1 <lukas.hilger@icloud.com>
2025-05-04 13:08:52 +0200

    Added ruby item group

---
+++ src/main/java/de/lulkas_/tutorial_mod_fabric/item/ModItemGroups.java
@@ -0,0 +1,31 @@
+package de.lulkas_.tutorial_mod_fabric.item;
+
+import de.lulkas_.tutorial_mod_fabric.TutorialModFabric;
+import de.lulkas_.tutorial_mod_fabric.block.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup RUBY_ITEMS = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TutorialModFabric.MOD_ID, "ruby_items"),
+            FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(ModItems.RUBY))
+                    .displayName(Text.translatable("itemgroup.tutorial_mod_fabric.ruby_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                        entries.add(ModItems.RAW_RUBY);
+                        entries.add(ModBlocks.RUBY_BLOCK);
+                        entries.add(ModBlocks.RAW_RUBY_BLOCK);
+                    })
+                    .build()
+    );
+
+    public static void registerModItemGroups() {
+        TutorialModFabric.LOGGER.info("Registering Mod Item Groups for " + TutorialModFabric.MOD_ID);
+    }
+}
