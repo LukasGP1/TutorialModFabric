2e99ea1281be0558a55585041cf4449b499ee16e
LukasGP1 <lukas.hilger@icloud.com>
2025-05-04 11:39:57 +0200

    Initial commit

---
+++ src/main/java/de/lulkas_/tutorial_mod_fabric/TutorialModFabric.java
@@ -0,0 +1,16 @@
+package de.lulkas_.tutorial_mod_fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialModFabric implements ModInitializer {
+	public static final String MOD_ID = "tutorial_mod_fabric";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/de/lulkas_/tutorial_mod_fabric/TutorialModFabricClient.java
@@ -0,0 +1,10 @@
+package de.lulkas_.tutorial_mod_fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
